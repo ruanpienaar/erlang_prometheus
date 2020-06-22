e08b0490a0177fb895e053b11b5d2c3e38771198
Ruan Pienaar <ruan800@gmail.com>
2020-06-22 19:24:01 +0100

    Initial commit - http port configurable

---
+++ src/erlang_prometheus_app.erl
@@ -0,0 +1,32 @@
+-module(erlang_prometheus_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+-include("erlang_prometheus.hrl").
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    Routes = [
+        {'_', [
+            {"/metrics/[:registry]", prometheus_cowboy2_handler, []}
+        ]}
+    ],
+    Dispatch = cowboy_router:compile(Routes),
+    HttpPort = application:get_env(erlang_prometheus, http_port, 8080),
+    {ok, _} = cowboy:start_clear(
+        http,
+        [{port, HttpPort}],
+        #{env => #{dispatch => Dispatch},
+        metrics_callback => fun prometheus_cowboy2_instrumenter:observe/1,
+        stream_handlers => [cowboy_metrics_h, cowboy_stream_h]}
+    ),
+    erlang_prometheus_sup:start_link().
+
+stop(_State) ->
+    ok.
